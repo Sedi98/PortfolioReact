57486476b6b6c50b9098b6955c6920a2fa5b56fc
Sedi98 <sedi9816@gmail.com>
2024-06-02 19:45:12 +0400

    Update project with new changes

--- src/components/SectionPortfolio.jsx
+++ src/components/SectionPortfolio.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import SectionTitleComponent from "./SectionTitleComponent";
 import ComponentPortfolioPromotion from "./ComponentPortfolioPromotion";
 
+// images
+import tibis from '../assets/img/tibis.webp'
+import bootstrap from '../assets/img/bootstrap.webp'
+import open_enterprise from '../assets/img/open_enterprise.webp'
+import clbarbers from '../assets/img/clbarbers.webp'
+import portfolio_old from '../assets/img/portfolio_old.webp'
+
 function SectionPortfolio() {
   return (
     <section id="portfolio" className="section_portfolio section_padding">
@@ -15,27 +22,27 @@ function SectionPortfolio() {
 
         <div className="flex_container flex_wrap">
           <ComponentPortfolioPromotion
-            url={"../src/assets/img/tibis.webp"}
+            url={tibis}
             linkRedirect={"https://tibis.az/"}
             linkShare={"https://tibis.az/"}
           />
           <ComponentPortfolioPromotion
-            url={"../src/assets/img/bootstrap.webp"}
+            url={bootstrap}
             linkRedirect={"https://sedi98.github.io/BootstrapConcept/"}
             linkShare={"https://sedi98.github.io/BootstrapConcept/"}
           />
           <ComponentPortfolioPromotion
-            url={"../src/assets/img/clbarbers.webp"}
+            url={clbarbers}
             linkRedirect={"https://sedi98.github.io/clbarbers/"}
             linkShare={"https://sedi98.github.io/clbarbers/"}
           />
           <ComponentPortfolioPromotion
-            url={"../src/assets/img/open_enterprise.webp"}
+            url={open_enterprise}
             linkRedirect={"https://sedi98.github.io/OpenEnterprise/"}
             linkShare={"https://sedi98.github.io/OpenEnterprise/"}
           />
           <ComponentPortfolioPromotion
-            url={"../src/assets/img/portfolio_old.webp"}
+            url={portfolio_old}
             linkRedirect={"https://sedi98.github.io/PortfolioProject/"}
             linkShare={"https://sedi98.github.io/PortfolioProject/"}
           />
